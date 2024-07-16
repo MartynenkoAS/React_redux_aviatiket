d2d92dcdf966ee592607b3a91cae177d86345204
Alexander Martynenko <MartynenkoAS@yandex.ru>
2024-07-16 22:13:23 +0300

    Tiket/ticket

--- src/components/Company.tsx
+++ src/components/Company.tsx
@@ -1,13 +1,14 @@
-import { useEffect } from "react";
-import ring_dark     from "../img/ring_purpure_full.png";
-import unring_dark   from "../img/ring_purpure_empty.png";
-import ring_light    from "../img/ring_white_full.png";
-import unring_light  from "../img/ring_white_empty.png";
-import mainTicketsData from './mockTiketData';
-import { useDispatch, useSelector } from 'react-redux';
 import { reducerInitCompany, reducerToggleCompany } from "../store/slices/companyChoiceSlice";
-import { Dispatch }  from '@reduxjs/toolkit';
-import { RootStore } from "../store/store";
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect }                from "react";
+import { Dispatch }                 from '@reduxjs/toolkit';
+import { RootStore }                from "../store/store";
+import { filterTickets }            from "../store/slices/ticketSlice";
+import ring_dark                    from "../img/ring_purpure_full.png";
+import unring_dark                  from "../img/ring_purpure_empty.png";
+import ring_light                   from "../img/ring_white_full.png";
+import unring_light                 from "../img/ring_white_empty.png";
+import mainTicketsData              from './mockTiketData';
 
 type PropsType = {colorScheme:boolean}
 type CompanyObjType = {[key: string]: boolean};
@@ -27,13 +28,18 @@ const Company = ( {colorScheme}: PropsType ) => {
     const ring:   string = colorScheme ? ring_light   : ring_dark;
     const unring: string = colorScheme ? unring_light : unring_dark;
 
+    const onChangeHandler = (elem: any) => {
+        dispatch(reducerToggleCompany(elem));
+        dispatch(filterTickets());
+    }
+
     return (
         <div className="trans_wrapper">
             <p className='aside-p'>Компании</p>
             
             {<div className='aside-label'>
                 {Object.keys(companySwitchObj).map((elem, index) => { return <label className='trans_label' key={"cc"+index}>
-                    <input type="checkbox" onChange={() => dispatch(reducerToggleCompany(elem))}/>
+                    <input type="checkbox" onChange={() => onChangeHandler(elem)}/>
                     {companySwitchObj[elem] ? <img className="company-checkbox" src={ring}   alt="../img/ring_purpure_full.png" /> : 
                                               <img className="company-checkbox" src={unring} alt="../img/ring_purpure_empty.png" />}
                     {elem}
--- src/store/slices/ticketSlice.tsx
+++ src/store/slices/ticketSlice.tsx
@@ -7,13 +7,19 @@ import mainTicketsData from '../../components/mockTiketData.ts';
 const initialState: {data: Ticket[], filterData: Ticket[]} = {data: mainTicketsData, filterData: mainTicketsData};
 
 const tiketSlice = createSlice({
-    name: "TiketData",
+    name: "ticketData",
     initialState,
     reducers: {
+        filterTickets: (state) => {
+            
+            
+        }
     },
 })
 
 
 // export const {} = TiketSlice.actions;
+
+export const {filterTickets} = tiketSlice.actions;
 
 export default tiketSlice;
